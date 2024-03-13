f0335cfbdaa6e8c754389e252f99a212d0a47e04
Satoki <satokiito@gmail.com>
2024-03-13 18:17:07 +0000

    feat: seed.js file updated with new profile

--- prisma/seed.js
+++ prisma/seed.js
@@ -6,6 +6,9 @@ async function seed() {
 
     const username = 'User'
     const password = 'Password123'
+    const first_name = 'John'
+    const last_name = 'Doe'
+    const display_name = 'musicman'
 
     const hashedPassword = await bcrypt.hash(password, 12)
 
@@ -16,7 +19,21 @@ async function seed() {
         }
     });
 
-    console.log('User created 1', user);
+    const profile = await prisma.profile.create({
+        data: {
+            first_name,
+            last_name,
+            display_name,
+            user: {
+                connect: {
+                    id: 1
+                }
+            }
+        }
+    })
+
+    console.log('User created', user)
+    console.log('Profile created', profile)
 
     process.exit(0);
 }
